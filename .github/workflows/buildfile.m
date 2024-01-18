dd431b251f31ac892dfcfa4fb5deb520992feadd
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2024-01-18 08:40:00 +0530

    Delete .github/workflows/buildfile.m

--- .github/workflows/buildfile.m
+++
@@ -1,8 +0,0 @@
-function plan = buildfile
-import matlab.buildtool.tasks.TestTask;
-
-plan = buildplan;
-plan("test") = TestTask(TestResults="test-results/results.xml");
-plan.DefaultTasks = "test";
-
-end
