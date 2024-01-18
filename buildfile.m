ab9d42d8663b3bc77f6515e59d57f13bd3c88a94
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2024-01-18 08:40:26 +0530

    Create buildfile.m at root

---
+++ buildfile.m
@@ -0,0 +1,8 @@
+function plan = buildfile
+import matlab.buildtool.tasks.TestTask;
+
+plan = buildplan;
+plan("test") = TestTask(TestResults="test-results/results.xml");
+plan.DefaultTasks = "test";
+
+end
