765d088e9a1052c575ff68960aecea22383b3433
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2024-07-04 11:44:38 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -2,7 +2,7 @@ function plan = buildfile
 import matlab.buildtool.tasks.TestTask;
 
 plan = buildplan;
-plan("test") = TestTask(TestResults="test-results/results.xml");
-% plan.DefaultTasks = "test";
+plan("test") = TestTask(SourceFiles="code", TestResults="test-results/results.xml");
+plan.DefaultTasks = "test";
 
 end
