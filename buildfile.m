b30c911765ec745a026a7c6d3e8bb2d5187b93a8
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2024-07-04 12:09:29 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -1,8 +1,13 @@
 function plan = buildfile
-import matlab.buildtool.tasks.TestTask;
+import matlab.buildtool.tasks.TestTask
 
-plan = buildplan;
-plan("test") = TestTask(SourceFiles="code");
-plan.DefaultTasks = "test";
+plan = buildplan(localfunctions);
+plan.DefaultTasks = "my";
 
+% Add a task to run the tests in the project
+plan("test") = TestTask(SourceFiles="code", TestResults="test-results/results.xml");
+end
+
+function myTask(~)
+    disp('Hello World!')
 end
