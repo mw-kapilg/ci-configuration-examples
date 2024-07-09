c0ae5c7643ec0969a7533573b2e28b08959d57d3
Kapil Gupta <111856426+mw-kapilg@users.noreply.github.com>
2024-07-09 11:17:38 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -4,8 +4,6 @@ import matlab.buildtool.tasks.TestTask
 plan = buildplan(localfunctions);
 plan.DefaultTasks = "my";
 
-% Add a task to run the tests in the project
-plan("test") = TestTask(SourceFiles="code", TestResults="test-results/results.xml");
 end
 
 function myTask(~)
